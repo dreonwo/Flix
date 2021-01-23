70ab95a1ea8ce8999b61cfc4b341fcf2940ebcbe
dreonwo <dwheatleyowens@icloud.com>
2021-01-22 23:13:16 -0500

    Added Landscape Functionality

--- app/src/main/java/com/example/flix/MainActivity.java
+++ app/src/main/java/com/example/flix/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+
+import android.annotation.SuppressLint;
+import android.app.ActionBar;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -30,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
         setContentView(R.layout.activity_main);
         RecyclerView rvmovies = findViewById(R.id.rvMovies);
         movies = new ArrayList<>();
--- app/src/main/java/com/example/flix/adapters/MovieAdapter.java
+++ app/src/main/java/com/example/flix/adapters/MovieAdapter.java
@@ -1,6 +1,8 @@
 package com.example.flix.adapters;
 
 import android.content.Context;
+import android.content.res.Configuration;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -30,6 +32,7 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
     @Override
     //Usually involves inflating a layout from XML and returning the holder
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Log.d("MovieAdapter", "onCreateViewHolder");
         View movieView = LayoutInflater.from(context).inflate(R.layout.item_movie, parent, false);
         return new ViewHolder(movieView);
     }
@@ -37,6 +40,7 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
     @Override
     //Involves populating data into the item through holder
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Log.d("MovieAdapter", "onBindViewHolder" + position);
         //Get the movie at the passed in position
         Movie movie = movies.get(position);
         //Bind the movie data into the VH
@@ -68,7 +72,16 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
         public void bind(Movie movie) {
             tvTitle.setText(movie.getTitle());
             tvOverview.setText(movie.getOverview());
-            Glide.with(context).load(movie.getPosterPath()).into(ivPoster);
+            String imageUrl;
+
+            if(context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+               imageUrl = movie.getBackdropPath();
+            }
+            else{
+                imageUrl = movie.getPosterPath();
+            }
+
+            Glide.with(context).load(imageUrl).into(ivPoster);
         }
     }
 }
